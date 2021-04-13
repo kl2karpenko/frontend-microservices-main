9dd84ada8a8ea413f13549113063d8b11bde2d5b
lkarpenko <lkarpenko@alteryx.com>
2021-04-13 17:53:42 +0300

    hello

---
+++ modules/section2/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div>Section 2</div>;
+};
+
+export default App;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div>
+    <div>Main App</div>
+
+  </div>;
+};
+
+export default App;
